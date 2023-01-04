35885087feedaa8353f97c06d3e9bc5f128b04be
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 07:25:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+        
+// Write Your Code Here
+
+LasInd = friends.pop();
+firstInd = friends.shift();
+
+console.log(friends); // ["Eman", "Osama"]
